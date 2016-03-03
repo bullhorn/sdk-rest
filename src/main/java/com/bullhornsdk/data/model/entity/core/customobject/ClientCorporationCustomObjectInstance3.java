9abc448ba64c4c5f1a6d6f0ce33fddf88cb1ead0
Ryan McDole <rmcdole@bullhorn.com>
2016-03-03 15:06:58 -0600

    moved clientCorporation field of clientCorporationCustomOjbectInstances into an abstract class

--- src/main/java/com/bullhornsdk/data/model/entity/core/customobject/ClientCorporationCustomObjectInstance3.java
+++ src/main/java/com/bullhornsdk/data/model/entity/core/customobject/ClientCorporationCustomObjectInstance3.java
@@ -1,6 +1,5 @@
  package com.bullhornsdk.data.model.entity.core.customobject;
 
- import com.bullhornsdk.data.model.entity.core.standard.ClientCorporation;
  import com.fasterxml.jackson.annotation.JsonInclude;
  import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  import com.fasterxml.jackson.annotation.JsonRootName;
@@ -11,16 +10,5 @@
          "text14", "text15", "text16", "text17", "text18", "text19", "text20", "int1", "int2", "int3", "int4", "int5", "int6", "int7", "int8", "int9",
          "int10", "float1", "float2", "float3", "float4", "float5", "float6", "float7", "float8", "float9", "float10", "textBlock1", "textBlock2",
          "textBlock3", "textBlock4", "textBlock5", "date1", "date2", "date3", "date4", "date5", "date6", "date7", "date8", "date9", "date10", "dateAdded" })
- public class ClientCorporationCustomObjectInstance3 extends CustomObjectInstance {
-
-     private ClientCorporation clientCorporation;
-
-     public ClientCorporation getClientCorporation() {
-         return clientCorporation;
-     }
-
-     public void setClientCorporation(ClientCorporation clientCorporation) {
-         this.clientCorporation = clientCorporation;
-     }
-
+ public class ClientCorporationCustomObjectInstance3 extends ClientCorporationCustomObjectInstance {
  }
