f3cb2efd3cdb87cdc6e467297eb71077b2b40b9b
Anita Wu <awu@bullhorn.com>
2016-08-11 15:36:54 -0700

    adding all methods to interface

--- src/main/java/com/bullhornsdk/data/model/file/FileMeta.java
+++ src/main/java/com/bullhornsdk/data/model/file/FileMeta.java
@@ -8,17 +8,31 @@ public interface FileMeta {
 
 	public Integer getId();
 
+	public void setId(Integer id);
+
 	public String getType();
+
+	public void setType(String type);
 
 	public String getName();
 
+	public void setName(String name);
+
 	public String getDescription();
+
+	public void setDescription(String description);
 
 	public String getContentType();
 
+	public void setContentType(String contentType);
+
 	public String getContentSubType();
 
+	public void setContentSubType(String contentSubType);
+
 	public String getFileType();
+
+	public void setFileType(String fileType);
 
 	public String getExternalID();
 
@@ -26,13 +40,19 @@ public interface FileMeta {
 
 	public String getDistribution();
 
+	public void setDistribution(String distribution);
+
 	public String getFileUrl();
+
+	public void setFileUrl(String fileUrl);
 
 	public String getFileContent();
 
 	public void setFileContent(String fileContent);
 
 	public DateTime getDateAdded();
+
+	public void setDateAdded(DateTime dateAdded);
 
 	public String getUrlString();
 
