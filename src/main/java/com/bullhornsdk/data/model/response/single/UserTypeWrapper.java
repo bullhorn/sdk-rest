a8a20c8f84d1a40acf7f3c84ccf576fe36e8ce84
John Sullivan <john.sullivan@bullhorn.com>
2016-11-03 13:11:23 -0400

    adding usertype to entityinfo enum with associated wrapper

---
+++ src/main/java/com/bullhornsdk/data/model/response/single/UserTypeWrapper.java
@@ -0,0 +1,7 @@
+package com.bullhornsdk.data.model.response.single;
+
+import com.bullhornsdk.data.model.entity.embedded.UserType;
+
+public class UserTypeWrapper extends StandardWrapper<UserType> {
+
+}
