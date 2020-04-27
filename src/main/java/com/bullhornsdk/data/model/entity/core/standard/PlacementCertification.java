ce5c92367e8b2d7058b48ac201583bd6d0ed0dbf
mkesmetzis <michael.kesmetzis@bullhorn.com>
2020-04-27 15:44:52 +0100

    updates Placement Certification entity - updated version

--- src/main/java/com/bullhornsdk/data/model/entity/core/standard/PlacementCertification.java
+++ src/main/java/com/bullhornsdk/data/model/entity/core/standard/PlacementCertification.java
@@ -1,42 +1,38 @@
 package com.bullhornsdk.data.model.entity.core.standard;
 
+import com.bullhornsdk.data.model.entity.core.certificationrequirement.AbstractRequirement;
+import com.bullhornsdk.data.model.entity.core.edithistory.EditHistory;
 import com.bullhornsdk.data.model.entity.core.type.*;
+import com.bullhornsdk.data.model.entity.embedded.OneToMany;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import org.joda.time.DateTime;
 
+import javax.validation.constraints.Size;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonRootName(value = "data")
-@JsonPropertyOrder({"id", "placement", "candidateCertification", "certification", "dateAdded", "modifyingUser", "dateLastModified"})
-public class PlacementCertification extends AbstractEntity implements UpdateEntity, CreateEntity, QueryEntity, DeleteEntity, AssociationEntity, EditHistoryEntity {
-
-    private Integer id;
+@JsonPropertyOrder({"id", "candidateCertification", "candidateCertificationName",
+    "candidateCertificationStatus", "candidate", "certification", "certificationFileAttachments",
+    "customDate1", "customDate10", "customDate2", "customDate3", "customDate4", "customDate5", "customDate6",
+    "customDate7", "customDate8", "customDate9", "customFloat1", "customFloat2", "customFloat3", "customInt1",
+    "customInt2", "customInt3", "customText1", "customText10", "customText2", "customText3",
+    "customText4", "customText5", "customText6", "customText7", "customText8", "customText9", "customTextBlock1",
+    "customTextBlock10", "customTextBlock2", "customTextBlock3", "customTextBlock4", "customTextBlock5", "customTextBlock6",
+    "customTextBlock7", "customTextBlock8", "customTextBlock9", "dateAdded", "dateExpiration", "dateLastModified", "documentDeadline",
+    "editHistories", "fileAttachments", "matchingCredentialCount", "modifyingUser", "owner", "status"})
+public class PlacementCertification extends AbstractRequirement implements UpdateEntity, CreateEntity, QueryEntity, DeleteEntity, AssociationEntity, EditHistoryEntity {
 
     private Placement placement;
-
-    private CandidateCertification candidateCertification;
-
-    private Certification certification;
-
-    private DateTime dateAdded;
-
-    private CorporateUser modifyingUser;
-
+    private OneToMany<EditHistory> editHistories;
     private DateTime dateLastModified;
+    @Size(max = 100)
+    private String candidateCertificationName;
+    @Size(max = 30)
+    private String candidateCertificationStatus;
 
-    @Override
-    @JsonProperty("id")
-    public Integer getId() {
-        return id;
-    }
-
-    @Override
-    @JsonProperty("id")
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     @JsonProperty("placement")
     public Placement getPlacement() {
@@ -48,33 +44,6 @@ public class PlacementCertification extends AbstractEntity implements UpdateEnti
         this.placement = placement;
     }
 
-    @JsonProperty("candidateCertification")
-    public CandidateCertification getCandidateCertification() {
-        return candidateCertification;
-    }
-
-    @JsonProperty("candidateCertification")
-    public void setCandidateCertification(CandidateCertification candidateCertification) { this.candidateCertification = candidateCertification; }
-
-    @JsonProperty("certification")
-    public Certification getCertification() {
-        return certification;
-    }
-
-    @JsonProperty("certification")
-    public void setCertification(Certification certification) {
-        this.certification = certification;
-    }
-
-    @JsonProperty("dateAdded")
-    public DateTime getDateAdded() {
-        return dateAdded;
-    }
-
-    @JsonProperty("dateAdded")
-    public void setDateAdded(DateTime dateAdded) {
-        this.dateAdded = dateAdded;
-    }
 
     @JsonProperty("dateLastModified")
     public DateTime getDateLastModified() {
@@ -86,60 +55,33 @@ public class PlacementCertification extends AbstractEntity implements UpdateEnti
         this.dateLastModified = dateLastModified;
     }
 
-    @JsonProperty("modifyingUser")
-    public CorporateUser getModifyingUser() {
-        return modifyingUser;
+    @JsonProperty("editHistories")
+    public OneToMany<EditHistory> getEditHistories() {
+        return editHistories;
     }
 
-    @JsonProperty("modifyingUser")
-    public void setModifyingUser(CorporateUser modifyingUser) {
-        this.modifyingUser = modifyingUser;
+    @JsonProperty("editHistories")
+    public void setEditHistories(OneToMany<EditHistory> editHistories) {
+        this.editHistories = editHistories;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PlacementCertification)) return false;
-
-        PlacementCertification that = (PlacementCertification) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (placement != null ? !placement.equals(that.placement) : that.placement != null) return false;
-        if (candidateCertification != null ? !candidateCertification.equals(that.candidateCertification) : that.candidateCertification != null) return false;
-        if (certification != null ? !certification.equals(that.certification) : that.certification != null)
-            return false;
-        if (dateAdded != null ? !dateAdded.equals(that.dateAdded) : that.dateAdded != null)
-            return false;
-        if (modifyingUser != null ? !modifyingUser.equals(that.modifyingUser) : that.modifyingUser != null)
-            return false;
-        if (dateLastModified != null ? !dateLastModified.equals(that.dateLastModified) : that.dateLastModified != null)
-            return false;
-        return true;
-
+    @JsonProperty("candidateCertificationName")
+    public String getCandidateCertificationName() {
+        return candidateCertificationName;
     }
 
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (placement != null ? placement.hashCode() : 0);
-        result = 31 * result + (candidateCertification != null ? candidateCertification.hashCode() : 0);
-        result = 31 * result + (certification != null ? certification.hashCode() : 0);
-        result = 31 * result + (dateAdded != null ? dateAdded.hashCode() : 0);
-        result = 31 * result + (modifyingUser != null ? modifyingUser.hashCode() : 0);
-        result = 31 * result + (dateLastModified != null ? dateLastModified.hashCode() : 0);
-        return result;
+    @JsonProperty("candidateCertificationName")
+    public void setCandidateCertificationName(String candidateCertificationName) {
+        this.candidateCertificationName = candidateCertificationName;
     }
 
-    @Override
-    public String toString() {
-        return "PlacementCertification{" +
-            "id=" + id +
-            ", placement='" + placement + '\'' +
-            ", candidateCertification=" + candidateCertification +
-            ", certification=" + certification +
-            ", dateAdded=" + dateAdded +
-            ", dateLastModified=" + dateLastModified +
-            ", modifyingUser=" + modifyingUser +
-            '}';
+    @JsonProperty("candidateCertificationStatus")
+    public String getCandidateCertificationStatus() {
+        return candidateCertificationStatus;
+    }
+
+    @JsonProperty("candidateCertificationStatus")
+    public void setCandidateCertificationStatus(String candidateCertificationStatus) {
+        this.candidateCertificationStatus = candidateCertificationStatus;
     }
 }
