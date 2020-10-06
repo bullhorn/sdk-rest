4e00d4d48379cd78a8adb15cc9eb2d2235738f33
Oleksandr Naumov <oleksandr.naumov@bullhorn.com>
2020-10-06 18:32:38 +0300

    added EarnCodeGroup

---
+++ src/main/java/com/bullhornsdk/data/model/entity/core/paybill/earncode/EarnCodeGroup.java
@@ -0,0 +1,216 @@
+package com.bullhornsdk.data.model.entity.core.paybill.earncode;
+
+import com.bullhornsdk.data.model.entity.core.paybill.generalledger.*;
+import com.bullhornsdk.data.model.entity.core.paybill.optionslookup.SimplifiedOptionsLookup;
+import com.bullhornsdk.data.model.entity.core.standard.CorporateUser;
+import com.bullhornsdk.data.model.entity.core.type.*;
+import com.bullhornsdk.data.model.entity.customfields.CustomFieldsA;
+import com.bullhornsdk.data.util.ReadOnly;
+import com.fasterxml.jackson.annotation.*;
+import org.joda.time.DateTime;
+
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+/**
+ * Created by onaumov 06-Oct-20
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonRootName(value = "data")
+@JsonPropertyOrder({"id", "accruesOT", "allowForSelfService", "chargeType", "dateAdded", "dateLastModified", "defaultEarnCode", "status",
+    "isTaxable", "overtimeEarnCode", "payBillOptions", "unitOfMeasure"})
+public class EarnCodeGroup extends CustomFieldsA implements QueryEntity, UpdateEntity, CreateEntity, DateLastModifiedEntity, EditHistoryEntity {
+
+    private Integer id;
+    private Boolean accruesOT;
+    private Boolean allowForSelfService;
+    private SimplifiedOptionsLookup chargeType;
+    private DateTime dateAdded;
+    private DateTime dateLastModified;
+    private EarnCode defaultEarnCode;
+    private EarnCode doubleTimeEarnCode;
+    private SimplifiedOptionsLookup status;
+    private Boolean isTaxable;
+    private EarnCode overtimeEarnCode;
+    private SimplifiedOptionsLookup payBillOptions;
+    private SimplifiedOptionsLookup unitOfMeasure;
+
+    @Override
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @ReadOnly
+    @Override
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("accruesOT")
+    public Boolean getAccruesOT() {
+        return accruesOT;
+    }
+
+    @JsonProperty("accruesOT")
+    public void setAccruesOT(Boolean accruesOT) {
+        this.accruesOT = accruesOT;
+    }
+
+    @JsonProperty("allowForSelfService")
+    public Boolean getAllowForSelfService() {
+        return allowForSelfService;
+    }
+
+    @JsonProperty("allowForSelfService")
+    public void setAllowForSelfService(Boolean allowForSelfService) {
+        this.allowForSelfService = allowForSelfService;
+    }
+
+    @JsonProperty("chargeType")
+    public SimplifiedOptionsLookup getChargeType() {
+        return chargeType;
+    }
+
+    @JsonProperty("chargeType")
+    public void setChargeType(SimplifiedOptionsLookup chargeType) {
+        this.chargeType = chargeType;
+    }
+
+    @JsonProperty("dateAdded")
+    public DateTime getDateAdded() {
+        return dateAdded;
+    }
+
+    @JsonProperty("dateAdded")
+    public void setDateAdded(DateTime dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    @JsonProperty("dateLastModified")
+    public DateTime getDateLastModified() {
+        return dateLastModified;
+    }
+
+    @ReadOnly
+    @JsonProperty("dateLastModified")
+    public void setDateLastModified(DateTime dateLastModified) {
+        this.dateLastModified = dateLastModified;
+    }
+
+    @JsonProperty("defaultEarnCode")
+    public EarnCode getDefaultEarnCode() {
+        return defaultEarnCode;
+    }
+
+    @JsonProperty("defaultEarnCode")
+    public void setDefaultEarnCode(EarnCode defaultEarnCode) {
+        this.defaultEarnCode = defaultEarnCode;
+    }
+
+    @JsonProperty("doubleTimeEarnCode")
+    public EarnCode getDoubleTimeEarnCode() {
+        return doubleTimeEarnCode;
+    }
+
+    @JsonProperty("doubleTimeEarnCode")
+    public void setDoubleTimeEarnCode(EarnCode doubleTimeEarnCode) {
+        this.doubleTimeEarnCode = doubleTimeEarnCode;
+    }
+
+    @JsonProperty("status")
+    public SimplifiedOptionsLookup getStatus() {
+        return status;
+    }
+
+    @JsonProperty("status")
+    public void setStatus(SimplifiedOptionsLookup status) {
+        this.status = status;
+    }
+
+    @JsonProperty("isTaxable")
+    public Boolean getTaxable() {
+        return isTaxable;
+    }
+
+    @JsonProperty("isTaxable")
+    public void setTaxable(Boolean taxable) {
+        isTaxable = taxable;
+    }
+
+    @JsonProperty("overtimeEarnCode")
+    public EarnCode getOvertimeEarnCode() {
+        return overtimeEarnCode;
+    }
+
+    @JsonProperty("overtimeEarnCode")
+    public void setOvertimeEarnCode(EarnCode overtimeEarnCode) {
+        this.overtimeEarnCode = overtimeEarnCode;
+    }
+
+    @JsonProperty("payBillOptions")
+    public SimplifiedOptionsLookup getPayBillOptions() {
+        return payBillOptions;
+    }
+
+    @JsonProperty("payBillOptions")
+    public void setPayBillOptions(SimplifiedOptionsLookup payBillOptions) {
+        this.payBillOptions = payBillOptions;
+    }
+
+    @JsonProperty("unitOfMeasure")
+    public SimplifiedOptionsLookup getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    @JsonProperty("unitOfMeasure")
+    public void setUnitOfMeasure(SimplifiedOptionsLookup unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        EarnCodeGroup earnCodeGroup = (EarnCodeGroup) o;
+        return Objects.equals(id, earnCodeGroup.id) &&
+            Objects.equals(accruesOT, earnCodeGroup.accruesOT) &&
+            Objects.equals(allowForSelfService, earnCodeGroup.allowForSelfService) &&
+            Objects.equals(chargeType, earnCodeGroup.chargeType) &&
+            Objects.equals(dateAdded, earnCodeGroup.dateAdded) &&
+            Objects.equals(dateLastModified, earnCodeGroup.dateLastModified) &&
+            Objects.equals(defaultEarnCode, earnCodeGroup.defaultEarnCode) &&
+            Objects.equals(doubleTimeEarnCode, earnCodeGroup.doubleTimeEarnCode) &&
+            Objects.equals(status, earnCodeGroup.status) &&
+            Objects.equals(isTaxable, earnCodeGroup.isTaxable) &&
+            Objects.equals(overtimeEarnCode, earnCodeGroup.overtimeEarnCode) &&
+            Objects.equals(payBillOptions, earnCodeGroup.payBillOptions) &&
+            Objects.equals(unitOfMeasure, earnCodeGroup.unitOfMeasure);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, accruesOT, allowForSelfService, chargeType, dateAdded, dateLastModified, defaultEarnCode, doubleTimeEarnCode, status, isTaxable, overtimeEarnCode, payBillOptions, unitOfMeasure);
+    }
+
+    @Override
+    public String toString() {
+        return "EarnCodeGroup{" +
+            "id=" + id +
+            ", accruesOT=" + accruesOT +
+            ", allowForSelfService=" + allowForSelfService +
+            ", chargeType=" + chargeType +
+            ", dateAdded=" + dateAdded +
+            ", dateLastModified=" + dateLastModified +
+            ", defaultEarnCode=" + defaultEarnCode +
+            ", doubleTimeEarnCode=" + doubleTimeEarnCode +
+            ", status=" + status +
+            ", isTaxable=" + isTaxable +
+            ", overtimeEarnCode=" + overtimeEarnCode +
+            ", payBillOptions=" + payBillOptions +
+            ", unitOfMeasure=" + unitOfMeasure +
+            '}';
+    }
+}
