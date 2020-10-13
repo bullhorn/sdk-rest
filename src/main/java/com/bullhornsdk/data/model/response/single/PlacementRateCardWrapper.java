cb14ecbc8e177d00aa6964d5c098be218a7f6791
ayman.saad-mis <ayman.saad-mis@bullhorn.com>
2020-10-13 16:09:38 +0200

    adding placementRateCard entities

---
+++ src/main/java/com/bullhornsdk/data/model/response/single/PlacementRateCardWrapper.java
@@ -0,0 +1,6 @@
+package com.bullhornsdk.data.model.response.single;
+
+import com.bullhornsdk.data.model.entity.core.standard.PlacementRateCard;
+
+public class PlacementRateCardWrapper extends StandardWrapper<PlacementRateCard> {
+}
