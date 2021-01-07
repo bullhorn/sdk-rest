8237c29205f4eae2b81308af10f9597d79229dda
Ahmed Samir <ahmed.samir@inovola.com>
2021-01-07 15:44:46 +0200

    create PlacementTimeAndExpenseEntity and add it to placement

---
+++ src/main/java/com/bullhornsdk/data/model/response/single/PlacementTimeAndExpenseWrapper.java
@@ -0,0 +1,6 @@
+package com.bullhornsdk.data.model.response.single;
+
+import com.bullhornsdk.data.model.entity.core.standard.PlacementTimeAndExpense;
+
+public class PlacementTimeAndExpenseWrapper extends StandardWrapper<PlacementTimeAndExpense> {
+}
