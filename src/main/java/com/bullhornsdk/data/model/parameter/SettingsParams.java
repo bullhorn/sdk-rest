ef0ed27408c6f4108252d3e81837bdd01334f1f2
John Sullivan <johnsully83@gmail.com>
2021-02-17 10:57:05 -0500

    Added new methods for passing privateLabelId to a getSettings call (#345)
    Added new methods for passing privateLabelId to a getSettings call (#345)
    Co-authored-by: John Sullivan <john.sullivan@bullhorn.net>

---
+++ src/main/java/com/bullhornsdk/data/model/parameter/SettingsParams.java
@@ -0,0 +1,9 @@
+package com.bullhornsdk.data.model.parameter;
+
+public interface SettingsParams extends RequestParameters {
+
+    Integer getPrivateLabelId();
+
+    void setPrivateLabelId(Integer privateLabelId);
+
+}
