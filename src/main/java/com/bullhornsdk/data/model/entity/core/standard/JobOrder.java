690cfbefaf7157ccc0a945bc557bf446627b95b4
Daniel Huber <daniel.huber@bullhorn.com>
2021-03-08 16:01:48 -0500

    added location properties to entities

--- src/main/java/com/bullhornsdk/data/model/entity/core/standard/JobOrder.java
+++ src/main/java/com/bullhornsdk/data/model/entity/core/standard/JobOrder.java
@@ -28,15 +28,15 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 		"customInt3", "customText1", "customText10", "customText11", "customText12", "customText13", "customText14", "customText15",
 		"customText16", "customText17", "customText18", "customText19", "customText2", "customText20", "customText3", "customText4",
 		"customText5", "customText6", "customText7", "customText8", "customText9", "customTextBlock1", "customTextBlock2",
-		"customTextBlock3", "customTextBlock4", "customTextBlock5", "dateAdded", "dateClosed", "dateEnd", "dateLastExported", "dateLastModified", "degreeList",
+		"customTextBlock3", "customTextBlock4", "customTextBlock5", "dateAdded", "dateClosed", "dateEnd", "dateLastExported", "dateLastModified", "dateLastPublished", "degreeList",
 		"description", "durationWeeks", "educationDegree", "employmentType", "externalCategoryID", "externalID", "feeArrangement",
 		"hoursOfOperation", "hoursPerWeek", "interviews", "isClientEditable", "isDeleted", "isInterviewRequired", "isJobcastPublished",
-		"isOpen", "isPublic", "jobBoardList", "notes", "numOpenings", "onSite", "opportunity", "optionsPackage", "owner", "payRate", "placements",
+		"isOpen", "isPublic", "jobBoardList", "markUpPercentage", "notes", "numOpenings", "onSite", "opportunity", "optionsPackage", "owner", "payRate", "placements",
 		"publicDescription", "publishedZip", "reasonClosed", "reportTo", "reportToClientContact", "responseUser", "salary", "salaryUnit",
 		"sendouts", "skillList", "skills", "source", "specialties", "startDate", "status", "submissions", "tasks", "taxRate", "taxStatus",
 		"tearsheets", "timeUnits", "title", "travelRequirements", "type", "usersAssigned", "webResponses", "willRelocate", "willRelocateInt",
         "willSponsor", "workersCompRate", "yearsRequired", "customObject1s", "customObject2s", "customObject3s", "customObject4s", "customObject5s",
-        "customObject6s", "customObject7s", "customObject8s", "customObject9s", "customObject10s"})
+        "customObject6s", "customObject7s", "customObject8s", "customObject9s", "customObject10s", "location"})
 public class JobOrder extends JobData implements QueryEntity, SearchEntity, UpdateEntity, CreateEntity, SoftDeleteEntity, FileEntity,
 		AssociationEntity, DateLastModifiedEntity, EditHistoryEntity {
 
