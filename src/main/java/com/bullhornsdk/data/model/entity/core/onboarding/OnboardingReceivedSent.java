792f64ed4dd23e5464cf057d0e90311d7ee7cf93
hubda <daniel.huber@bullhorn.com>
2021-10-14 13:46:58 -0400

    added JsonProperty tags

--- src/main/java/com/bullhornsdk/data/model/entity/core/onboarding/OnboardingReceivedSent.java
+++ src/main/java/com/bullhornsdk/data/model/entity/core/onboarding/OnboardingReceivedSent.java
@@ -2,14 +2,12 @@ package com.bullhornsdk.data.model.entity.core.onboarding;
 
 import com.bullhornsdk.data.model.entity.core.type.AbstractEntity;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import java.util.Objects;
 
-/**
- * Created by mkesmetzis 24-Apr-20
- */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonRootName(value = "data")
 @JsonPropertyOrder({"onboardingDocumentReceivedCount", "onboardingDocumentSentCount"})
@@ -22,18 +20,22 @@ public class OnboardingReceivedSent extends AbstractEntity {
     public OnboardingReceivedSent() {
     }
 
+    @JsonProperty("onboardingDocumentReceivedCount")
     public Integer getOnboardingDocumentReceivedCount() {
         return onboardingDocumentReceivedCount;
     }
 
+    @JsonProperty("onboardingDocumentReceivedCount")
     public void setOnboardingDocumentReceivedCount(Integer onboardingDocumentReceivedCount) {
         this.onboardingDocumentReceivedCount = onboardingDocumentReceivedCount;
     }
 
+    @JsonProperty("onboardingDocumentSentCount")
     public Integer getOnboardingDocumentSentCount() {
         return onboardingDocumentSentCount;
     }
 
+    @JsonProperty("onboardingDocumentSentCount")
     public void setOnboardingDocumentSentCount(Integer onboardingDocumentSentCount) {
         this.onboardingDocumentSentCount = onboardingDocumentSentCount;
     }
