f6e2bd11d62c6c00c30d8dfa204243b35c39d0cc
Mohamed Mahmoud <mohamed.mahmoud-mis@bullhorn.com>
2022-04-27 18:23:48 +0200

    add: CandidateAvailability Entity

---
+++ src/main/java/com/bullhornsdk/data/model/response/single/CandidateAvailabilityWrapper.java
@@ -0,0 +1,7 @@
+package com.bullhornsdk.data.model.response.single;
+
+import com.bullhornsdk.data.model.entity.core.standard.CandidateAvailability;
+
+public class CandidateAvailabilityWrapper extends StandardWrapper<CandidateAvailability> {
+
+}
