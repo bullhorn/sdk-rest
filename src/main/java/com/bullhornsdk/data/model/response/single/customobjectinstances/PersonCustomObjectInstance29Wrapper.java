f154e18e7a23058188d090ec4cd5fbb7eb149ec2
Ayman Saad <ayman_saad1991@yahoo.com>
2023-01-03 15:48:06 +0200

    adding support for person CO29 (#402)
    Co-authored-by: ayman.saad-mis <ayman.saad-mis@bullhorn.com>

---
+++ src/main/java/com/bullhornsdk/data/model/response/single/customobjectinstances/PersonCustomObjectInstance29Wrapper.java
@@ -0,0 +1,7 @@
+package com.bullhornsdk.data.model.response.single.customobjectinstances;
+
+import com.bullhornsdk.data.model.entity.core.customobjectinstances.person.PersonCustomObjectInstance29;
+import com.bullhornsdk.data.model.response.single.StandardWrapper;
+
+public class PersonCustomObjectInstance29Wrapper  extends StandardWrapper<PersonCustomObjectInstance29> {
+}
