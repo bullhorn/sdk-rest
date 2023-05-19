025e7c0f5ade929cbb97537b2f737cb010d6b8eb
Trevor Swartz <trevor.swartz@bullhorn.com>
2023-05-19 11:14:41 -0400

    Initial fixing up of RestJsonConverterTest

--- src/test/java/com/bullhornsdk/data/api/helper/RestJsonConverterTest.java
+++ src/test/java/com/bullhornsdk/data/api/helper/RestJsonConverterTest.java
@@ -1,19 +1,59 @@
 package com.bullhornsdk.data.api.helper;
 
 import com.bullhornsdk.data.BaseTest;
+import com.bullhornsdk.data.exception.RestMappingException;
 import com.bullhornsdk.data.model.entity.core.standard.Candidate;
+import com.bullhornsdk.data.model.entity.core.standard.JobSubmission;
+import com.bullhornsdk.data.model.enums.BullhornEntityInfo;
+import com.bullhornsdk.data.model.response.file.standard.StandardFileContent;
+import com.bullhornsdk.data.model.response.list.ListWrapper;
+import com.bullhornsdk.data.model.response.single.StandardFileContentWrapper;
+import com.bullhornsdk.data.model.response.single.StandardWrapper;
+import com.bullhornsdk.data.model.response.single.Wrapper;
 import com.google.common.collect.Sets;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RestJsonConverterTest extends BaseTest {
+    private RestJsonConverter restJsonConverter = new RestJsonConverter();
     private Candidate candidate;
 
+    private final String singleEntityJson = "{ \"data\": { \"id\": 1, \"status\":\"Approved\" }}";
+
+    private final String singleEntityJsonExtraProp = "{ " +
+        "\"total\": 1, " +
+        "\"data\": { \"id\": 1, \"status\":\"Approved\" }}";
+
+    private final String multipleEntityJson = "{ " +
+        "\"data\": [" +
+        "{ \"id\": 1, \"status\":\"Approved\" }, " +
+        "{ \"id\": 2, \"status\":\"Not Submitted\" }" +
+        "]}";
+
+    private final String multipleEntityJsonExtraProp = "{ " +
+        "\"total\": 2, " +
+        "\"data\": [" +
+        "{ \"id\": 1, \"status\":\"Approved\" }, " +
+        "{ \"id\": 2, \"status\":\"Not Submitted\" }" +
+        "]}";
+
+    private final String standardFileContentJson = "{ " +
+        "\"File\": { \"fileContent\": \"SomeContent\", \"name\":\"FileName\" }}";
+
+    private final String standardFileContentJsonExtraProp = "{ " +
+        "\"total\": 1, " +
+        "\"File\": { \"fileContent\": \"SomeContent\", \"name\":\"FileName\" }}";
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         candidate = bullhornData.findEntity(Candidate.class, testEntities.getCandidateId(), Sets.newHashSet("id", "firstName"));
     }
 
@@ -31,62 +71,23 @@ public class RestJsonConverterTest extends BaseTest {
         JSONObject expected = new JSONObject("{\"id\": 1,\"firstName\": \"Want\", \"lastName\": null}");
         JSONObject result = new JSONObject(jsonConverter.convertEntityToJsonString(candidate, Sets.newHashSet("lastName")));
         assertTrue("JSON conversion includes unexpected fields, or does not include expected fields", expected.similar(result));
-import com.bullhornsdk.data.exception.RestMappingException;
-import com.bullhornsdk.data.model.entity.core.standard.JobSubmission;
-import com.bullhornsdk.data.model.enums.BullhornEntityInfo;
-import com.bullhornsdk.data.model.response.file.standard.StandardFileContent;
-import com.bullhornsdk.data.model.response.list.ListWrapper;
-import com.bullhornsdk.data.model.response.single.StandardFileContentWrapper;
-import com.bullhornsdk.data.model.response.single.StandardWrapper;
-import com.bullhornsdk.data.model.response.single.Wrapper;
-import org.junit.Test;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-public class RestJsonConverterTest {
-    RestJsonConverter restJsonConverter = new RestJsonConverter();
-
-    private final String singleEntityJson = "{ \"data\": { \"id\": 1, \"status\":\"Approved\" }}";
-
-    private final String singleEntityJsonExtraProp = "{ " +
-        "\"total\": 1, " +
-        "\"data\": { \"id\": 1, \"status\":\"Approved\" }}";
-
-    private final String multipleEntityJson = "{ " +
-        "\"data\": [" +
-            "{ \"id\": 1, \"status\":\"Approved\" }, " +
-            "{ \"id\": 2, \"status\":\"Not Submitted\" }" +
-        "]}";
-
-    private final String multipleEntityJsonExtraProp = "{ " +
-        "\"total\": 2, " +
-        "\"data\": [" +
-        "{ \"id\": 1, \"status\":\"Approved\" }, " +
-        "{ \"id\": 2, \"status\":\"Not Submitted\" }" +
-        "]}";
-
-    private final String standardFileContentJson = "{ " +
-        "\"File\": { \"fileContent\": \"SomeContent\", \"name\":\"FileName\" }}";
-
-    private final String standardFileContentJsonExtraProp = "{ " +
-        "\"total\": 1, " +
-        "\"File\": { \"fileContent\": \"SomeContent\", \"name\":\"FileName\" }}";
+    }
 
     @Test
     public void jsonToEntityTest() {
         Wrapper<JobSubmission> jobSubmissionWrapper = this.restJsonConverter.jsonToEntity(this.singleEntityJson, BullhornEntityInfo.getTypesWrapperType(JobSubmission.class));
         assertNotNull(jobSubmissionWrapper);
         assertNotNull(jobSubmissionWrapper.getData());
-        assertEquals(jobSubmissionWrapper.getData().getId(), new Integer(1));
-        assertEquals(jobSubmissionWrapper.getData().getStatus(), "Approved");
+        assertEquals(1, jobSubmissionWrapper.getData().getId());
+        assertEquals("Approved", jobSubmissionWrapper.getData().getStatus());
     }
 
-    @Test(expected = RestMappingException.class)
+    @Test
     public void jsonToEntityMultipleException() {
-        this.restJsonConverter.jsonToEntity(this.singleEntityJson, BullhornEntityInfo.getTypesListWrapperType(JobSubmission.class));
+        assertThrows(RestMappingException.class, () ->
+            this.restJsonConverter.jsonToEntity(this.singleEntityJson, BullhornEntityInfo.getTypesListWrapperType(JobSubmission.class))
+        );
+
     }
 
     @Test
@@ -94,7 +95,7 @@ public class RestJsonConverterTest {
         StandardWrapper<JobSubmission> jobSubmissionWrapper = this.restJsonConverter.jsonToEntity(this.singleEntityJsonExtraProp, BullhornEntityInfo.getTypesWrapperType(JobSubmission.class));
         assertNotNull(jobSubmissionWrapper);
         assertNotNull(jobSubmissionWrapper.getData());
-        assertEquals(new Integer(1), jobSubmissionWrapper.getData().getId());
+        assertEquals(1, jobSubmissionWrapper.getData().getId());
         assertEquals("Approved", jobSubmissionWrapper.getData().getStatus());
         assertEquals(1, jobSubmissionWrapper.getAdditionalProperties().get("total"));
     }
@@ -102,10 +103,17 @@ public class RestJsonConverterTest {
     @Test
     public void testJsonListToEntitiesWithExtraParentProperty() {
         ListWrapper<JobSubmission> jobSubmissionWrapper = this.restJsonConverter.jsonToEntity(this.multipleEntityJsonExtraProp, BullhornEntityInfo.getTypesListWrapperType(JobSubmission.class));
+
         assertNotNull(jobSubmissionWrapper);
         assertNotNull(jobSubmissionWrapper.getData());
-        assertEquals(new Integer(2), jobSubmissionWrapper.getTotal());
+        assertEquals(2, jobSubmissionWrapper.getTotal());
         assertEquals(2, jobSubmissionWrapper.getData().size());
+
+        List<JobSubmission> jobSubmissionList = jobSubmissionWrapper.getData();
+        assertEquals(1, jobSubmissionList.get(0).getId());
+        assertEquals("Approved", jobSubmissionList.get(0).getStatus());
+        assertEquals(2, jobSubmissionList.get(1).getId());
+        assertEquals("Not Submitted", jobSubmissionList.get(1).getStatus());
     }
 
     @Test
@@ -114,15 +122,17 @@ public class RestJsonConverterTest {
         assertNotNull(jobSubmissionWrapper);
         assertNotNull(jobSubmissionWrapper.getData());
         List<JobSubmission> jobSubmissionList = jobSubmissionWrapper.getData();
-        assertEquals(new Integer(1), jobSubmissionList.get(0).getId());
+        assertEquals(1, jobSubmissionList.get(0).getId());
         assertEquals("Approved", jobSubmissionList.get(0).getStatus());
-        assertEquals(new Integer(2), jobSubmissionList.get(1).getId());
+        assertEquals(2, jobSubmissionList.get(1).getId());
         assertEquals("Not Submitted", jobSubmissionList.get(1).getStatus());
     }
 
-    @Test(expected = RestMappingException.class)
+    @Test
     public void testJsonToMultipleEntityWithSingleDataWrapper() {
-        this.restJsonConverter.jsonToEntity(this.multipleEntityJson, BullhornEntityInfo.getTypesWrapperType(JobSubmission.class));
+        assertThrows(RestMappingException.class, () ->
+            this.restJsonConverter.jsonToEntity(this.multipleEntityJson, BullhornEntityInfo.getTypesWrapperType(JobSubmission.class))
+        );
     }
 
     @Test
@@ -144,6 +154,4 @@ public class RestJsonConverterTest {
         assertEquals("SomeContent", file.getFileContent());
         assertEquals("FileName", file.getName());
     }
-
-
 }
