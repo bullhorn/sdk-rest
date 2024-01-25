00d8773edf58caa61a5ab10d368c841592a6859b
Tomás El Fakih <tomaselfakih@gmail.com>
2024-01-25 17:09:11 -0400

    PS-40584: Removing wrong RestOneToManySerializer usage

--- src/main/java/com/bullhornsdk/data/model/entity/core/paybill/rate/ClientCorporationRateAgreementCardLine.java
+++ src/main/java/com/bullhornsdk/data/model/entity/core/paybill/rate/ClientCorporationRateAgreementCardLine.java
@@ -1,16 +1,11 @@
 package com.bullhornsdk.data.model.entity.core.paybill.rate;
 
-import com.bullhornsdk.data.api.helper.RestOneToManySerializer;
 import com.bullhornsdk.data.api.helper.json.DynamicNullValueFilter;
 import com.bullhornsdk.data.model.entity.core.paybill.earncode.EarnCode;
-import com.bullhornsdk.data.model.entity.core.paybill.earncode.EarnCodeGroup;
 import com.bullhornsdk.data.model.entity.core.paybill.unit.CurrencyUnit;
 import com.bullhornsdk.data.model.entity.core.type.*;
 import com.bullhornsdk.data.model.entity.customfields.CustomFieldsI;
-import com.bullhornsdk.data.model.entity.embedded.OneToMany;
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.joda.time.DateTime;
@@ -28,7 +23,6 @@ public class ClientCorporationRateAgreementCardLine extends CustomFieldsI implem
     BigDecimal billMultiplier;
     BigDecimal billRate;
     DateTime dateLastModified;
-    @JsonSerialize(using = RestOneToManySerializer.class)
     ClientCorporationRateAgreementCardLineGroup clientCorporationRateAgreementCardLineGroup;
     EarnCode earnCode;
     BigDecimal markupPercent;
