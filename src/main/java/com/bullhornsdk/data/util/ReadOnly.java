a5673b72c4c8f34e7a78c47959d6af2f76ea18b9
Trevor Swartz <swartz.trevor24@gmail.com>
2024-02-20 13:48:26 -0500

    Removing WriteOnly restriction on serialization for fields annotated with ReadOnly (#454)

--- src/main/java/com/bullhornsdk/data/util/ReadOnly.java
+++ src/main/java/com/bullhornsdk/data/util/ReadOnly.java
@@ -1,8 +1,5 @@
 package com.bullhornsdk.data.util;
 
-import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,7 +7,5 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
-@JacksonAnnotationsInside
-@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 public @interface ReadOnly {
 }
