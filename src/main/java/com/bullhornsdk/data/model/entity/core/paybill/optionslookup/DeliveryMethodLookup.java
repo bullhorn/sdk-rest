7c13d29000be12a1d7afa1206a4ff75f70288ca8
Leemar Scott <leemar.scott@bullhorn.com>
2025-12-19 10:38:17 -0600

    BH-98761 - line clean up

--- src/main/java/com/bullhornsdk/data/model/entity/core/paybill/optionslookup/DeliveryMethodLookup.java
+++ src/main/java/com/bullhornsdk/data/model/entity/core/paybill/optionslookup/DeliveryMethodLookup.java
@@ -7,9 +7,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
-/**
- * Created by claude 2025
- */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonRootName(value = "data")
 @JsonPropertyOrder({"id", "label", "isDeleted", "description", "isSystem", "isHidden", "shouldShowInPicker",
